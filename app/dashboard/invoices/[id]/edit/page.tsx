a9ca05b9b55b8a526fbca12edd804247a82756de
semanavasco <semanavasco@gmail.com>
2025-07-17 22:00:50 +0200

    Chapter 13 of next's AppRouter course

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 export default async function Page(prop: { params: Promise<{ id: string }> }) {
   const params = await prop.params;
@@ -10,6 +11,8 @@ export default async function Page(prop: { params: Promise<{ id: string }> }) {
     fetchInvoiceById(id),
     fetchCustomers()
   ]);
+
+  if (!invoice) notFound();
 
   return (
     <main>
