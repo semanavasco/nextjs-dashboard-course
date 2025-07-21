4419ad79b710d83240eb460f3364e4368798c990
semanavasco <semanavasco@gmail.com>
2025-07-21 21:36:07 +0200

    Chapter 15 of next's AppRouter course

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,9 +1,27 @@
 "use server";
 
+import { AuthError } from "next-auth";
+import { signIn } from "@/auth";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
 import { z } from "zod";
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+};
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
